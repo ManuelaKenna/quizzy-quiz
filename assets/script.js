1721e1e583dcd85b17d1a6764d1eae20aa42359b
Manuelacreate <107735377+Manuelacreate@users.noreply.github.com>
2022-07-29 19:14:47 -0700

    added a working timer

--- assets/script.js
+++ assets/script.js
@@ -3,18 +3,36 @@ var startEl=document.querySelector(".start")
 var startBtn=document.querySelector(".start-btn")
 var qi=0
 
+
+//what the start button is doing
 startBtn.addEventListener("click",function(){
     startEl.setAttribute("class", "hide")
     quizEl.removeAttribute("class","hide")
     displayQuestion()
 })
+
+//add a timer
+var count = 15;
+var interval = setInterval(function(){
+  document.getElementById('count').innerHTML=count;
+  count--;
+  if (count === 0){
+    clearInterval(interval);
+    document.getElementById('count').innerHTML='Done';
+    alert("You're out of time!");
+  }
+}, 1000);
+
+//change to real questions
 var questions=[
-    {text:"What is html?",choices:["a","b","c","d"],answer:"a"},
+    {text:"Commonly used data types do not include:",choices:["a. booleans","b. strings","c. alerts","d. numbers"],answer:"a"},
     {text:"What is html?",choices:["a","b","c","d"],answer:"a"},
     {text:"What is html?",choices:["a","b","c","d"],answer:"a"},
     {text:"What is html?",choices:["a","b","c","d"],answer:"a"},
     {text:"What is html?",choices:["a","b","c","d"],answer:"a"}
 ]
+
+//display first question
 function displayQuestion(){
     document.querySelector(".question-text").textContent=questions[qi].text
     document.querySelector('.answer-box').innerHTML=""
